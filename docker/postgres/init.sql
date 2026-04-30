a1756aa0b4281e66d44577aab4d4808b16c4f543
ADExcelec <a.diaz@excelec.com>
2026-04-30 10:13:18 -0500

    fix(env) files

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,8 +1,8 @@
 -- Script ejecutado una sola vez al inicializar el contenedor de PostgreSQL.
 -- Crea las bases de datos para la API y para Airflow si no existen.
 
-SELECT 'CREATE DATABASE pipeline'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'pipeline')\gexec
+SELECT 'CREATE DATABASE turismo_academico_medellin'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'turismo_academico_medellin')\gexec
 
 SELECT 'CREATE DATABASE airflow'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow')\gexec
